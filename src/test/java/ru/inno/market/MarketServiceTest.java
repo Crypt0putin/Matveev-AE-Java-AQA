be1b33cba7ea9efbc7bdd9ba18f18fde5c0b532f
a.matveev <a.matveev@andersenlab.com>
2023-07-03 00:07:10 +0300

    ololo

--- src/test/java/ru/inno/market/MarketServiceTest.java
+++ src/test/java/ru/inno/market/MarketServiceTest.java
@@ -1,4 +1,22 @@
 package ru.inno.market;
 
+import org.junit.jupiter.api.DisplayName;
+import ru.inno.market.core.MarketService;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
+import ru.inno.market.model.Order;
+import ru.inno.market.model.PromoCodes;
+import java.util.NoSuchElementException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class MarketServiceTest {
+
+    MarketService service;
+
+    @Test
+    @DisplayName("Создается один заказ")
+    public void ShouldCreateOrder() {
+
+    }
 }
--- src/test/java/ru/inno/market/OrderTest.java
+++ src/test/java/ru/inno/market/OrderTest.java
@@ -1,4 +1,29 @@
 package ru.inno.market;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import ru.inno.market.core.Catalog;
+import ru.inno.market.model.Client;
+import ru.inno.market.model.Order;
+
+import static org.junit.Assert.assertEquals;
+
 public class OrderTest {
+
+    Order order;
+    int testOrderId = 1;
+    Client client;
+    int testItemId = 2;
+    Catalog catalog = new Catalog();
+
+    @BeforeEach
+    public void setUp() {
+        client = new Client(1, "Artur");
+        order = new Order(testOrderId, client);
+    }
+
+    @Test
+    public void ShouldGetId() {
+        assertEquals(testOrderId, order.getId());
+    }
 }
