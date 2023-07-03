3af678c7dd7c7cfa1a735b2f3d5b7886d5bcf343
Crypt0putin <72546982+Crypt0putin@users.noreply.github.com>
2023-07-03 22:17:39 +0300

    The 2nd ver.

--- src/test/java/ru/inno/market/OrderTest.java
+++ src/test/java/ru/inno/market/OrderTest.java
@@ -1,10 +1,15 @@
 package ru.inno.market;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.*;
 import ru.inno.market.core.Catalog;
 import ru.inno.market.model.Client;
+import ru.inno.market.model.Item;
 import ru.inno.market.model.Order;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,8 +27,26 @@ public class OrderTest {
         order = new Order(testOrderId, client);
     }
 
+
+    @DisplayName("Корректная очистка корзины при новом заказе")
     @Test
-    public void ShouldGetId() {
+    public void ShouldHaveEmptyCartForTheNewOrders() {
+        Map<Item, Integer> expectedMap = new HashMap<>();
+        var actualMap = order.getCart();
+
+        assertEquals(expectedMap, actualMap);
+    }
+
+    @DisplayName("Корректный возврат id заказа")
+    @Test
+    public void ShouldGetId()
+    {
         assertEquals(testOrderId, order.getId());
+    }
+
+    @DisplayName("Корректный возврат id клиента")
+    @Test
+    public void shouldGetClient() {
+        assertEquals(client, order.getClient());
     }
 }
